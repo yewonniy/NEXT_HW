2baa71f0157726a7cfe2d030da22c2725d76ce30
yewonniy <ae6652@naver.com>
2023-05-04 22:34:35 +0900

    session14

--- session6/portfolioProject/myapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def info(request):
-    return render(request, 'info.html')
-
-def project(request):
-    return render(request, 'project.html')
